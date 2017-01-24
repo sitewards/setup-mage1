36fcafcce035d7e5e21a5bff2e583f4e5e676dd9
ironm <poposki.darko@gmail.com>
2017-01-24 21:01:06 +0000

    Initial commit of the Magento1 bridge

---
+++ Application/Bridge.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the Setup package.
+ *
+ * (c) Sitewards GmbH
+ */
+
+namespace Sitewards\SetupMage1\Application;
+
+use Sitewards\Setup\Application\BridgeInterface;
+use Sitewards\Setup\Domain\Page\PageRepositoryInterface;
+
+class Bridge implements BridgeInterface
+{
+    public function __construct()
+    {
+        $this->initMagento();
+    }
+
+    /**
+     * Init the magento bootstrap and create the cli app and object manager
+     */
+    private function initMagento()
+    {
+        $projectRootDir = __DIR__ . '/../../../..';
+
+        if (file_exists($projectRootDir . '/web/app/Mage.php')) {
+            require $projectRootDir . '/web/app/Mage.php';
+        }
+
+        \Mage::app();
+    }
+
+    /**
+     * @return PageRepositoryInterface
+     */
+    public function getPageRepository()
+    {
+        return new \Sitewards_Setup_Model_Repository_PageRepository(
+            \Mage::getModel('cms/page')->getCollection()
+        );
+    }
+}
---
+++ app/code/community/Sitewards/Setup/Model/Repository/PageRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+use Sitewards\Setup\Domain\Page\Page;
+use Sitewards\Setup\Domain\Page\PageRepositoryInterface;
+
+class Sitewards_Setup_Model_Repository_PageRepository implements PageRepositoryInterface
+{
+    /**
+     * @var \Mage_Cms_Model_Resource_Page_Collection
+     */
+    private $pageCollection;
+
+    public function __construct(\Mage_Cms_Model_Resource_Page_Collection $pageCollection)
+    {
+        $this->pageCollection = $pageCollection;
+    }
+
+    public function findByIds(array $ids)
+    {
+        $this->pageCollection->addFieldToFilter('identifier', [ 'in' => $ids ]);
+
+        return $this->findItems();
+    }
+
+    public function findAll()
+    {
+        return $this->findItems();
+    }
+
+    private function findItems()
+    {
+        $pages = [];
+
+        /** @var \Mage_Cms_Model_Page $page */
+        foreach ($this->pageCollection as $page) {
+            $pages[] = new Page(
+                $page->getIdentifier(),
+                $page->getTitle(),
+                $page->getContent(),
+                $page->getIsActive()
+            );
+        }
+
+        return $pages;
+    }
+
+    public function import(Page $page)
+    {
+        /** @var Mage_Cms_Model_Page $oPage */
+        $oPage = Mage::getModel('cms/page');
+        $oResource = $oPage->getResource();
+        $iPageId = $oResource->checkIdentifier($page->getIdentifier(), 0);
+        $oPage->load($iPageId);
+        $oPage->setIdentifier($page->getIdentifier());
+        $oPage->setTitle($page->getTitle());
+        $oPage->setContent($page->getContent());
+        $oPage->setIsActive($page->getActive());
+        $oPage->save();
+    }
+}
